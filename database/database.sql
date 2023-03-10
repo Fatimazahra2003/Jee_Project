3e3de7ab4e7cbfd6d96a1a00451f2465487909b7
Fatimazahra2003 <fatimazahrawahia@gmail.com>
2023-03-10 22:36:13 +0100

    updated

---
+++ database/database.sql
@@ -0,0 +1,6 @@
+create table Users_Project_JEE(
+                               idU number primary key,
+                               username varchar2(50),
+                               email varchar2(100),
+                               password_user varchar2(50)
+                               );
